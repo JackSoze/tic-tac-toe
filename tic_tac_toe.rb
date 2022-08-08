4645ee00f9b63e97477a984f1627a5394af9e733
jacksoze <kamonyojack@gmail.com>
2022-08-08 11:03:38 +0300

    Create the board positions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,24 +5,31 @@ end
 
 # this is the game
 class Tictactoe
+  attr_accessor :positions
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     greeting
+    @positions = %w[1,2,3,4,5,6,7,8,9]
+    positions_dis
   end
 
-  private
+  # private
+  def positions_dis
+    puts @positions
+  end
 
   def greeting
     puts 'welcome to the game Tic-Tac-Toe'
-    sleep 1.5
+    # sleep 1.5
     player_names
   end
 
   def player_names
     puts 'please enter player 1 name'
     @player1.name = gets.chomp
-    sleep 1.5
+    # sleep 1.5
     puts 'please enter player 2 name'
     @player2.name = gets.chomp
   end
